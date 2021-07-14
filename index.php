83ffe5202022b1f3d173df4234008d5585356397
fateemah200 <fatimaaminumusa7@gmail.com>
2021-07-14 21:13:59 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRMS</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="main">
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About us</a></li>
+        <li><a href="#">Contact us</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Services</a></li>
+    </ul>
+</nav>
+<div class="container">
+<h1>Sweet Coffee</h1>
+<p>Resgister with us and have a cup coffe on your table every morning.</p>
+<p class="go">Want to know more about us..</p>
+</div>
+    </div>
+</body>
+</html>
