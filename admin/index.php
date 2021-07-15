0a7e78096f48c4953be8e24adbe8a829cb8fc775
KabeerIsahZaybarh <Princezayba017@gmail.com>
2021-07-15 11:55:50 +0100

    kabeer isah

---
+++ admin/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="bootstrap/fontawesome/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/fontawesome/css/font-awesome.css">
+     <link rel="stylesheet" type="text/css" href="bootstrap/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="bootstrap/botstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="bootstrap/botstrap/css/bootstrap.min.css">
+    <script src="bootstrap/wow/wow.js"></script>
+    <script src="bootstrap/fontawesome/js/all.min.js"></script>
+     <script src="bootstrap/fontawesome/js/fontawesome.min.js"></script>
+    <title>login-- CRMS</title>
+</head>
+<body>
+  <div class="container" id="main">
+<div class="sign-in">
+    <form action="" method="">
+      <h1>Sign in</h1>
+      <input type="email" name="email" placeholder="your email" required> 
+      <input type="password" name="password" placeholder="your password" required> 
+      <a href="#">forget password</a>
+      <button>sign in</button> 
+    </form>
+</div>
+<div class="overlay-container">
+    <div class="overlay">
+         <div class="overlay-right">
+            <h1>Welcome....</h1>
+            <p>Don't have account with us</p>
+            <button id="signUp">sign up</button>
+        </div>
+        
+    </div>
+</div>
+  </div> 
+ 
+</body>
+</html>
