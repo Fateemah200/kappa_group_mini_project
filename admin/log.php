3a7f740d77d3afe3b58f62fda9bd91c47c05ca67
Hashimhabib296 <86925148+Hashimhabib296@users.noreply.github.com>
2021-07-18 12:56:03 +0100

    hashim hashim habib

--- admin/log.php
+++ admin/log.php
@@ -54,7 +54,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;                            
                             
                      
-                            header("location:home.php");
+                            header("location: home.php");
                         } else{
                    
                             $login_err = "Invalid username or password.";
@@ -77,47 +77,3 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     unset($pdo);
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="bootstrap/fontawesome/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="bootstrap/fontawesome/css/font-awesome.css">
-     <link rel="stylesheet" type="text/css" href="bootstrap/fontawesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="bootstrap/botstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="bootstrap/botstrap/css/bootstrap.min.css">
-    <script src="bootstrap/wow/wow.js"></script>
-    <script src="bootstrap/fontawesome/js/all.min.js"></script>
-     <script src="bootstrap/fontawesome/js/fontawesome.min.js"></script>
-    <title>login-- CRMS</title>
-</head>
-<body>
-  <div class="container" id="main">
-<div class="sign-in">
-
-    <form action="" method="post">
-      <h1>Sign in</h1>
-      <input type="text" name="username" placeholder="your username" required>
-
-      <input type="password" name="password" placeholder="your password" required> 
-      <a href="#">forget password</a>
-      <button>sign in</button> 
-    </form>
-</div>
-<div class="overlay-container">
-    <div class="overlay">
-         <div class="overlay-right">
-            <h1>Welcome....</h1>
-            <p>Don't have account with us</p>
-            <button id="signUp">sign up</button>
-        </div>
-        
-    </div>
-</div>
-  </div> 
- 
-</body>
-</html>
